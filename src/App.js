5d393ae4704e42c8de131a22395b11909dc5bbe6
Donaldo Azemaj <donaldazemaj@hotmail.com>
2021-05-24 21:23:44 +0200

    init

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi Donald Azemaj
         </p>
         <a
           className="App-link"
